0d6d4a2ab94e3264d3a58a2efe7be2eedc480967
richieoey <richie.oey88@gmail.com>
2022-12-05 17:50:36 +0700

    commit store db

--- app/src/main/java/id/ac/umn/uas_43802/StoreAdapter.java
+++ app/src/main/java/id/ac/umn/uas_43802/StoreAdapter.java
@@ -1,67 +1,5 @@
 package id.ac.umn.uas_43802;
-//
-//import android.content.Context;
-//import android.media.Image;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> {
-//
-//	private Context context;
-//	private List<StoreModel> list;
-//
-//	public class ViewHolder extends RecyclerView.ViewHolder {
-//		TextView tvNama;
-//		ImageView imgStore;
-//		public ViewHolder(@NonNull View itemView) {
-//			super(itemView);
-//
-//			tvNama = itemView.findViewById(R.id.store_name);
-//			imgStore = itemView.findViewById(R.id.image_store);
-//		}
-//	}
-//
-//	public StoreAdapter(Context context, List<StoreModel> list){
-//		this.context = context;
-//		this.list = list;
-//	}
-//
-//	@NonNull
-//	@Override
-//	public StoreAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.store_item_list, parent, false);
-//		return new ViewHolder(view);
-//	}
-//
-//	@Override
-//	public void onBindViewHolder(@NonNull StoreAdapter.ViewHolder holder, int position) {
-//		TextView txtNama = holder.tvNama;
-//		ImageView ivStore = holder.imgStore;
-//
-//		txtNama.setText(list.get(position).getName());
-//		ivStore.setImageResource(R.drawable.anjay_store);
-//	}
-//
-//	@Override
-//	public int getItemCount() {
-//		return list.size();
-//	}
-//
-//}
-
-
-
-		import android.content.Context;
+import android.content.Context;
 		import android.view.LayoutInflater;
 		import android.view.View;
 		import android.view.ViewGroup;
@@ -109,7 +47,7 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder
 	public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
 		RequestOptions options = new RequestOptions();
-		options.circleCrop();
+		options.fitCenter();
 		Glide.with(context).load(data.get(position).getImage()).apply(options).into(holder.image);
 		holder.name.setText(data.get(position).getName());
 	}
