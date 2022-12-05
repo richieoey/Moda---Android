695546928ccbd1107e86b640df7818bdd16328af
richieoey <richie.oey88@gmail.com>
2022-12-05 21:11:05 +0700

    chat layout

--- app/src/main/java/id/ac/umn/uas_43802/StoreDetail.java
+++ app/src/main/java/id/ac/umn/uas_43802/StoreDetail.java
@@ -9,16 +9,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import id.ac.umn.uas_43802.databinding.ActivityLoginBinding;
+import id.ac.umn.uas_43802.databinding.ActivityStoreDetailBinding;
+
 public class StoreDetail extends AppCompatActivity {
     ImageView btnBack, btnCart, imGambarStore;
     TextView tvNameToolbar, tvStoreName, tvProvince;
     RecyclerView rvProduk;
     CardView btnChatPenjual;
+    ActivityStoreDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_store_detail);
+        binding = ActivityStoreDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         btnBack = findViewById(R.id.backbtn_store_detail);
         btnCart = findViewById(R.id.add_cart_toko);
         imGambarStore = findViewById(R.id.image_store_detail);
